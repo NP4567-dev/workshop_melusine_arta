6037049a1ece1f0f7a2d06707cf8df03f2fea13e
develop-cs <43383361+develop-cs@users.noreply.github.com>
2025-03-06 07:39:51 +0100

    chore: remove some contents
    Signed-off-by: develop-cs <43383361+develop-cs@users.noreply.github.com>

--- assets/actions.py
+++
@@ -1,8 +0,0 @@
-def set_priority(value, **kwargs):
-    return {"priority": value}
-
-
-def send_reply(reply_id, **kwargs):
-    # Fake rest api call
-    print(f"The following email model was automatically sent: {reply_id}")
-    return reply_id
